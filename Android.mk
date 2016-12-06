darjwx/android_hardware_qcom_display

Allow msm8952 platform to use msm8994 HAL
Our msm8952 platform devices work fine with the msm8994 HAL.

Change-Id: I79d2a15405c893410530e27a08bcb3ed57c8006f
Signed-off-by: Adam Farden <adam@farden.cz>

Android.mk
@@ -14,7 +14,7 @@ else
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8960)
 else
-ifneq ($(filter msm8916 msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8916 msm8952 msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8994)
 else
 ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)

8974-Sony-Project/hardware-qcom-display

Android.mk
@@ -10,7 +10,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
         include $(call all-named-subdir-makefiles,msm8960)
       else
-        ifneq ($(filter msm8974 msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+        ifneq ($(filter msm8974 msm8952 msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
           include $(call all-named-subdir-makefiles,msm8994)
         else
           ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)